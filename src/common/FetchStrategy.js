eb9334f99e15024158c2ac4d051eba5e51ec5bb9
adamchuk <adamchuk@b38a3b43-5428-0410-89d0-e338276489da>
2007-02-26 14:26:40 +0000

    Many changes. Fixed problem with "Single tab". Improved code.

--- src/common/FetchStrategy.js
+++
@@ -1,8 +0,0 @@
-var FetchStrategy = 
-{
-	QueryUrl : "http://cemetery.org.ua/tmp/.htcabinet/index.cgi",
-	
-	prepareXml : function(data) {return new DataContainer(data);},
-	
-	getMethodType : function() {return "GET"}
-}

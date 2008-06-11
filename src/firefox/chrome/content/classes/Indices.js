8323c57f1c61f2a5175819763101ac0432bbdcab
adamchuk <adamchuk@b38a3b43-5428-0410-89d0-e338276489da>
2008-06-11 11:13:58 +0000

    Added indicatiors for input and output west

--- src/firefox/chrome/content/classes/Indices.js
+++ src/firefox/chrome/content/classes/Indices.js
@@ -4,6 +4,8 @@ PowerCab.Indices = {
 		{img:"warning.png", path:'/opt/limit/@limit', bit:0x2, label:"indlabel.limit"},
 		{img:"in.gif", path:'/opt/traf_cnt[@name="total"]/@rx', bit:0x4, label:"indlabel.input"},
 		{img:"out.gif", path:'/opt/traf_cnt[@name="total"]/@tx', bit:0x8, label:"indlabel.output"},
+		{img:"worldin.png", path:'/opt/traf_cnt[@name="west"]/@rx', bit:0x100, label:"indlabel.worldinput"},
+		{img:"worldout.png", path:'/opt/traf_cnt[@name="west"]/@tx', bit:0x200, label:"indlabel.worldoutput"},
 		{img:"world.png", path:'/opt/traf_cnt[@name="west"]/@sum', bit:0x10, label:"indlabel.west"},
 		{img:"ukraine.gif", path:'/opt/traf_cnt[@name="ua"]/@sum', bit:0x20, label:"indlabel.ukraine"},
 		{img:"video.png", path:'/opt/traf_cnt[@name="media"]/@sum', bit:0x40, label:"indlabel.uamedia"},
